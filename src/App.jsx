48541c5cb0ce2e919e6fe29b0d008efe1c6750a2
anyway404 <kevin.hasler@bluewin.ch>
2024-06-01 12:09:23 +0200

    route-update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Ersatzwagen from "./pages/Ersatzwagen.jsx";
 import Mietwagen from "./pages/Mietwagen.jsx";
 import Impressum from "./pages/Impressum.jsx";
 import Login from "./pages/Login.jsx";
-import NoPage from "./pages/NoPage.jsx";
 
 export default function App() {
   const isAuthenticated = localStorage.getItem("token");
@@ -21,33 +20,34 @@ export default function App() {
             />
             <Route
               path="/aktuell"
-              element={isAuthenticated ? <Aktuell /> : <Navigate to="/login" />}
+              element={
+                isAuthenticated ? <Aktuell /> : <Navigate to="/aktuell" />
+              }
             />
             <Route
               path="/impressum"
               element={
-                isAuthenticated ? <Impressum /> : <Navigate to="/login" />
+                isAuthenticated ? <Impressum /> : <Navigate to="/impressum" />
               }
             />
             <Route
               path="/ersatzwagen"
               element={
-                isAuthenticated ? <Ersatzwagen /> : <Navigate to="/login" />
+                isAuthenticated ? (
+                  <Ersatzwagen />
+                ) : (
+                  <Navigate to="/ersatzwagen" />
+                )
               }
             />
             <Route
               path="/mietwagen"
               element={
-                isAuthenticated ? <Mietwagen /> : <Navigate to="/login" />
+                isAuthenticated ? <Mietwagen /> : <Navigate to="/mietwagen" />
               }
             />
-            <Route
-              path="/login"
-              element={
-                !isAuthenticated ? <Login /> : <Navigate to="/aktuell" />
-              }
-            />
-            <Route path="*" element={<NoPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<Login />} />
           </Routes>
         </BrowserRouter>
       </div>
